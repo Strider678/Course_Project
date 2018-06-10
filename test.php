295a6e613fec2420a5827af11e403f63ffee5700
Strider678 <35690396+Strider678@users.noreply.github.com>
2018-06-10 13:44:38 +0300

    Update test.php

--- test.php
+++ test.php
@@ -43,7 +43,7 @@
 <body>
   <div align="center">
     <h1>Congratulations!</h1>
-    <h2>This application was deployed using AWS CodeDeploy from GitHub.</h2>
+    <h2>This is test of Auto Deployment.</h2>
     <h1>Version 1.0</h1>
     <?php
       $id = file_get_contents("http://instance-data/latest/meta-data/instance-id");
